4d516c2e6ef9b615c1fb08aef682f5c10d9026c6
Nick Wong <njwon4@gmail.com>
2023-01-19 14:38:38 +1100

    chore: init repo and add SVGs

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- TODO: Implement the pricing page here -->
+    <h1>Welcome!</h1>
+    <!-- SVG example -->
+    <UiSvg name="migaku-logo-color" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+// CSS reset
+@import url('@/assets/styles/preflight.css');
+
+// Fonts & typography
+@import url('@/assets/fonts/GT-Maru/gt-maru.css');
+@import url('@/assets/fonts/Inter/inter.css');
+@import url('@/assets/styles/typography.css');
+
+// Theme
+@import url('@/assets/styles/colors.css');
+
+// Mixins
+@import url('@/assets/styles/mixins.scss')
+</style>
